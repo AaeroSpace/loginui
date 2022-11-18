c87d83e8c0c3f7e7785641eef8389c3142da6cdf
Aaron Warren <98857623+AaeroSpace@users.noreply.github.com>
2022-11-18 00:16:53 -0600

    Add files via upload

--- lib/schedule_page.dart
+++ lib/schedule_page.dart
@@ -83,23 +83,38 @@ class _SchedulerPage extends State<Scheduler> {
               //Class and Section Header Row
               Row(children: [
                 Container(
-                  child: Text('Class'),
+                  child: Text(
+                    'Class',
+                    style: TextStyle(
+                      color: Color.fromRGBO(37, 57, 92, 1.0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
                   padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                   width: 90,
                 ),
                 Expanded(
                     child: Container(
-                  height: 50,
+                  height: 70,
                   width: 30,
                 )),
                 Container(
-                  child: Text('Section #'),
-                  width: 80,
+                  child: Text(
+                    'Section #',
+                    style: TextStyle(
+                      color: Color.fromRGBO(37, 57, 92, 1.0),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  width: 115,
                 )
               ]),
               //CSC 1351 button
               InkWell(
                 onTap: () {
+                  //Class Information Slider
                   showModalBottomSheet<void>(
                     backgroundColor: Colors.transparent,
                     context: context,
@@ -113,13 +128,130 @@ class _SchedulerPage extends State<Scheduler> {
                                 topRight: Radius.circular(20))),
                         child: Center(
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Text('Modal BottomSheet'),
-                              ElevatedButton(
-                                child: const Text('Close BottomSheet'),
-                                onPressed: () => Navigator.pop(context),
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 15, 0, 3),
+                                child: Text(
+                                  'CSC 1350',
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(37, 57, 92, 1.0),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Computer Science I for CSC Majors',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(37, 57, 92, 1.0),
+                                  fontSize: 17,
+                                ),
+                              ),
+                              const Divider(
+                                height: 25,
+                                thickness: 3,
+                                color: Color.fromARGB(112, 187, 187, 187),
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 25, vertical: 0)),
+                                  Column(
+                                    children: [
+                                      //Time Slot
+                                      Icon(Icons.access_time_filled),
+                                      Text(
+                                        '9:20am -\n10:20am',
+                                        style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(37, 57, 92, 1.0),
+                                          fontSize: 16,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 38, vertical: 0)),
+                                  Column(
+                                    children: [
+                                      //Days Slot
+                                      Icon(Icons.calendar_month),
+                                      Text(
+                                        'M,W,F\n',
+                                        style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(37, 57, 92, 1.0),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 35, vertical: 0)),
+                                  Column(
+                                    //Professor Slot
+                                    children: [
+                                      Icon(Icons.account_circle_outlined),
+                                      Text(
+                                        'Skark, E\n',
+                                        style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(37, 57, 92, 1.0),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const Divider(
+                                height: 25,
+                                thickness: 3,
+                                color: Color.fromARGB(112, 187, 187, 187),
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 0)),
+                                  Text(
+                                    'Pre-req:',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(37, 57, 92, 1.0),
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Divider(
+                                height: 25,
+                                thickness: 3,
+                                color: Color.fromARGB(112, 187, 187, 187),
+                              ),
+                              Column(
+                                children: [
+                                  Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 50)),
+                                  SizedBox(
+                                    child: Text(
+                                      'This course teached you the fundamentals of algorithm development,'
+                                      ' program design and structured programming using an object-oriented'
+                                      ' language.',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(37, 57, 92, 1.0),
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    width: 400,
+                                  )
+                                ],
                               ),
                             ],
                           ),
@@ -131,13 +263,25 @@ class _SchedulerPage extends State<Scheduler> {
                 child: Container(
                     child: Row(
                       children: [
-                        Text('CSC 1351'),
+                        Text(
+                          'CSC 1350',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                         Expanded(child: Container()),
-                        Text('1'),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                       ],
                     ),
-                    height: 50,
-                    width: 380,
+                    height: 60,
+                    width: 390,
                     decoration: BoxDecoration(
                       color: _elevatedButtonColor,
                       borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -146,7 +290,7 @@ class _SchedulerPage extends State<Scheduler> {
                         EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
               ),
               SizedBox(
-                height: 6,
+                height: 11,
               ),
               //Math 1551 button
               InkWell(
@@ -177,13 +321,25 @@ class _SchedulerPage extends State<Scheduler> {
                 child: Container(
                     child: Row(
                       children: [
-                        Text('MATH 1551'),
+                        Text(
+                          'MATH 1550',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                         Expanded(child: Container()),
-                        Text('1'),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                       ],
                     ),
-                    height: 50,
-                    width: 380,
+                    height: 60,
+                    width: 390,
                     decoration: BoxDecoration(
                       color: _elevatedButtonColor,
                       borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -192,7 +348,7 @@ class _SchedulerPage extends State<Scheduler> {
                         EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
               ),
               SizedBox(
-                height: 6,
+                height: 11,
               ),
               //Engl 1001 button
               InkWell(
@@ -223,13 +379,25 @@ class _SchedulerPage extends State<Scheduler> {
                 child: Container(
                     child: Row(
                       children: [
-                        Text('ENGL 1001'),
+                        Text(
+                          'ENGL 1001',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                         Expanded(child: Container()),
-                        Text('1'),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                       ],
                     ),
-                    height: 50,
-                    width: 380,
+                    height: 60,
+                    width: 390,
                     decoration: BoxDecoration(
                       color: _elevatedButtonColor,
                       borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -238,8 +406,7 @@ class _SchedulerPage extends State<Scheduler> {
                         EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
               ),
               SizedBox(
-                height: 6,
-                width: 380,
+                height: 11,
               ),
               //ASTR 1001 button
               InkWell(
@@ -270,13 +437,25 @@ class _SchedulerPage extends State<Scheduler> {
                 child: Container(
                     child: Row(
                       children: [
-                        Text('ASTR 1001'),
+                        Text(
+                          'BIOL 1001',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                         Expanded(child: Container()),
-                        Text('1'),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Color.fromRGBO(37, 57, 92, 1.0),
+                            fontSize: 24,
+                          ),
+                        ),
                       ],
                     ),
-                    height: 50,
-                    width: 380,
+                    height: 60,
+                    width: 390,
                     decoration: BoxDecoration(
                       color: _elevatedButtonColor,
                       borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -284,6 +463,26 @@ class _SchedulerPage extends State<Scheduler> {
                     padding:
                         EdgeInsets.symmetric(horizontal: 20, vertical: 15)),
               ),
+              SizedBox(
+                height: 230,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Confirm Schedule',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(239, 169, 58, 1.0),
+                  shape: RoundedRectangleBorder(
+                      //to set border radius to button
+                      borderRadius: BorderRadius.circular(25)),
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                ),
+              )
             ],
           ),
         ),
