b0d5b08134d8d650b37eca93f66038eda38bda24
rbellsil <116689050+rbellsil@users.noreply.github.com>
2022-11-22 10:44:37 -0600

    get rid of back arrow

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -20,200 +20,187 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       extendBodyBehindAppBar:true,
-       body: Container(
-        height: double.infinity,
-        width: double.infinity,
-       
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('images/lsu-6288-campus-before-storm.jpg'),
-          fit: BoxFit.cover),
-        ),
-        child: SafeArea(
-          child: Column(
+      extendBodyBehindAppBar: true,
+      body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/lsu-6288-campus-before-storm.jpg'),
+                fit: BoxFit.cover),
+          ),
+          child: SafeArea(
+              child: Column(children: [
+            // mainAxisAlignment: MainAxisAlignment.end,
+            Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                ]),
+            Row(
               children: [
-                // mainAxisAlignment: MainAxisAlignment.end,
-                Row(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    SizedBox(
-                      height: 50,
-                    ),
-                  ]
-                ),
-                Row(
-                children: [
-                  SizedBox(
-                    width: 30,
-                  ),
-                  SizedBox(
-                    height: 60,
-                    width: 335,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        ),
-                        child: Text(
-                          'CSC Second Discipline',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Fall22()),
-                          );
-                        },
-                      ),
-                    ),
-                   ],
-              ),
-              Row(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                  ]
-                ),
-                Row(
-                children: [
-                  SizedBox(
-                    width: 30,
-                  ),
-                  SizedBox(
-                    height: 60,
-                    width: 335,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        ),
-                        child: Text(
-                          'Freshman',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Fall22()),
-                          );
-                        },
-                      ),
-                    ),
-                   ],
-              ),
-              Row(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                  ]
-                ),
-                Row(
-                children: [
-                  SizedBox(
-                    width: 30,
-                  ),
-                  SizedBox(
-                    height: 60,
-                    width: 335,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        ),
-                        child: Text(
-                          'Started: Fall 2022',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Fall22()),
-                          );
-                        },
-                      ),
-                    ),
-                   ],
-              ),
-              Row(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    SizedBox(
-                      height: 70,
-                    ),
-                  ]
-                ),
-              
-              Column(
-                children: [
-                Center(
-                  child:
                 SizedBox(
-                    height:70,
-                    width: 250,
-                    child: Center(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFEFA93A),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        ),
-                        child: Text(
-                          'Go to Schedule',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Fall22()),
-                          );
-                        },
+                  width: 30,
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 335,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    ),
+                    child: Text(
+                      'CSC Second Discipline',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 27,
                       ),
                     ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Fall22()),
+                      );
+                    },
                   ),
-                )
-                ]
-              ), 
-          ]
-          )
-        )
-      ),
+                ),
+              ],
+            ),
+            Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                ]),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 335,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    ),
+                    child: Text(
+                      'Freshman',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 27,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Fall22()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                ]),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30,
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 335,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(37, 57, 92, 1.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    ),
+                    child: Text(
+                      'Started: Fall 2022',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 27,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Fall22()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  SizedBox(
+                    height: 70,
+                  ),
+                ]),
+
+            Column(children: [
+              Center(
+                child: SizedBox(
+                  height: 70,
+                  width: 250,
+                  child: Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFEFA93A),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                      ),
+                      child: Text(
+                        'Go to Schedule',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Fall22()),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              )
+            ]),
+          ]))),
       appBar: AppBar(
         toolbarHeight: 250,
+        automaticallyImplyLeading: false,
         title: Container(
           child: Column(
             // ignore: prefer_const_literals_to_create_immutables
@@ -284,7 +271,6 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
         centerTitle: true,
         backgroundColor: Color.fromARGB(180, 255, 255, 255),
-        
       ),
     );
   }
